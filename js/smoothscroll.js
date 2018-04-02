704553d19c9bf8c330dc7c9bb369f5e3e5acce9c
Shaun <xshaunx1@gmail.com>
2018-04-02 14:14:31 -0500

    Fixed smooth scroll

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,26 +1,10 @@
-$(document).ready(function() {
-  // Add smooth scrolling to all links
-  $("a").on("click", function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+$(document).on("click", 'a[href^="#"]', function(event) {
+  event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top
-        },
-        800,
-        function() {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        }
-      );
-    } // End if
-  });
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top - 64
+    },
+    1000
+  );
 });
